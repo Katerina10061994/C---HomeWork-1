61c9754af5f67966400fc4532fc8ad57ac8b8f56
Katerina <katerina.lapitskaia@gmail.com>
2023-07-14 11:24:21 +0700

    home work 1 - task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,21 +1,13 @@
-﻿// Console.WriteLine("Введите число a");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите число b");
-// int b = Convert.ToInt32(Console.ReadLine());
-// if (a > b)
-//    Console.WriteLine($"Большее число a - {a}, меньшее число b - {b}");
-//    else Console.WriteLine($"Большее число b - {b}, меньшее число a - {a}");
-
-// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
 // а какое меньшее.
 //  a = 5; b = 7 -> max = 7
 //  a = 2 b = 10 -> max = 10
 //  a = -9 b = -3 -> max = -3
 
-
-int a = -9;
-int b = -3;
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
-    Console.WriteLine($"Большее число a: {a}, меньшее число b: {b}");
-else Console.WriteLine($"Большее число b: {b}, меньшее число a: {a}");
-
+    Console.WriteLine($"Большее число a = {a}, меньшее число b = {b}");
+else Console.WriteLine($"Большее число b = {b}, меньшее число a = {a}");
